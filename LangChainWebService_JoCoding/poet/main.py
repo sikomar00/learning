3cb126e64bd9ea8b8372d9366ccf652a4f981af9
sikomar00 <123606516+sikomar00@users.noreply.github.com>
2023-11-27 09:03:17 +0900

    1127

--- LangChainWebService_JoCoding/poet/main.py
+++ LangChainWebService_JoCoding/poet/main.py
@@ -1,2 +1,16 @@
 from dotenv import load_dotenv #dotenv 임포트
 load_dotenv()
+
+#.env를 안 쓰는 경우
+# from langchain.chat_models import ChatOpenAI
+# llm = ChatOpenAI(openai_api_key="...")
+
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+llm = OpenAI()
+chat_model = ChatOpenAI()
+
+llm.predict("Hi!")
+
+chat_model.predict("Hi!")
